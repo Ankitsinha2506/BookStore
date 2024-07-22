2e695cdb62b556dd028260b0eb447388ebac8b2e
Ankit8210 <ankitsinha25061909@gmail.com>
2024-07-22 13:15:11 +0530

    Home Page Ready

--- frontend/src/components/FreeBook.jsx
+++ frontend/src/components/FreeBook.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React from 'react';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import list from '../assets/list.json';
+import Card from './Card';
 
 const FreeBook = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const filterData = list.filter((data) => data.category === 'Free');
 
-export default FreeBook
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+    return (<>
+        <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
+            <div>
+                <h1 className='font-semibold text-xl pb-2'>Free Offered Courses</h1>
+                <p>Raaz chhupe hain har panaah mein,
+                    Khojne wale ko milte hain raah mein."</p>
+            </div>
+
+
+            <div>
+                <Slider {...settings}>
+                    {filterData.map((item) => (
+                        <Card item={item} key={item.id} />
+                    ))}
+                </Slider>
+            </div>
+        </div>
+    </>
+    );
+};
+
+export default FreeBook;
